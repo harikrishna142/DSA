8784f14d9ba1229b307603f1d4978ef1787f7268
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-02-02 01:45:11 -0500

    Time: 4 ms (41.76%) | Memory: 17.9 MB (25.76%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -7,9 +7,9 @@ class Solution:
         m=i+(j-i)//2
         while i<=j:
             if m*m>x:
-                j=int(m)-1
+                j=m-1
             elif m*m<x:
-                i=int(m)+1
+                i=m+1
             else:
                 return int(m)
             m=i+(j-i)//2
