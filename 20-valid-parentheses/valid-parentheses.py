13288163982145fff8932c67d52528f14e96577c
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-02-04 13:10:27 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (42.16%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        f={"}":"{","]":"[",")":"("}
+        r=[]
+        for i in range(len(s)):
+            if s[i] in f:
+                if len(r)==0 or f[s[i]]!=r[-1]:
+                    return False
+                elif f[s[i]]==r[-1]:
+                    r.pop()
+            else:
+                r.append(s[i])
+        if len(r)==0:
+            return True
+        else:
+            return False
+        
+                    
+
+
+        
