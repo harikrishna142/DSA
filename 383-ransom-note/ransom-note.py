c6ce7310e6154e9602395a8fc0e3e549acacfb6b
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-02-05 20:36:41 -0500

    Time: 15 ms (76.56%) | Memory: 18 MB (46.77%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -5,11 +5,9 @@ class Solution:
         #for i in range(len(magazine)):
         #    g[magazine[i]]+=1
         for i in range(len(ransomNote)):
-            if ransomNote[i] in g:
-                if g[ransomNote[i]]>=1:
+            if ransomNote[i] in g and g[ransomNote[i]]>=1:
                     g[ransomNote[i]]-=1
-                else:
-                    return False
+
             else:
                 return False
         return True
