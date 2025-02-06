367ac17296ea971eb0b604fd42cb4108a6ea2ec1
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-02-05 20:35:36 -0500

    Time: 15 ms (76.56%) | Memory: 18 MB (46.77%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,9 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        g=collections.defaultdict(int)
-        #f=Counter(magazine)
-        for i in range(len(magazine)):
-            g[magazine[i]]+=1
+        #g=collections.defaultdict(int)
+        g=Counter(magazine)
+        #for i in range(len(magazine)):
+        #    g[magazine[i]]+=1
         for i in range(len(ransomNote)):
             if ransomNote[i] in g:
                 if g[ransomNote[i]]>=1:
