4f6d7ad241aa718fd12fed75285323a00730e6b0
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-02-12 21:07:28 -0500

    Time: 10 ms (33.98%) | Memory: 21.5 MB (86.44%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        m=len(board)
+        n=len(board[0])
+        def dfs(i,j):
+            if i<0 or j<0 or i>m-1 or j>n-1 or board[i][j]!="O":
+                return
+            board[i][j]="-1"
+            dfs(i,j-1)
+            dfs(i,j+1)
+            dfs(i-1,j)
+            dfs(i+1,j)
+
+        for i in range(m):
+            for j in range(n):
+                if i in [0,m-1] or j in [0,n-1]:
+                    if board[i][j]=="O":
+                        dfs(i,j)
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]=="-1":
+                    board[i][j]="O"
+                else:
+                    board[i][j]="X"
+        
+            
+
+            
+
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
