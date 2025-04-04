a4ea8bbfe8fa28989acd353c8399feb96b9ba97f
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-04-04 17:11:25 -0400

    Time: 7 ms (7.99%) | Memory: 20 MB (5.80%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,41 +1,18 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        i=0
-        f=0
-        if len(intervals)==0:
-            intervals.append(newInterval)
-            return intervals
+        intervals.append(newInterval)
+        intervals.sort()
+        i=1
         while i<len(intervals):
-            if newInterval[0]>intervals[i][0] and f==0 and len(intervals)==1:
-                intervals.append(newInterval)
-                f=1
-                i+=1
-            
-            elif newInterval[0]<=intervals[i][0] and f==0:
-                if i==0:
-                    intervals.insert(i,newInterval)
-                    i+=1
-                if intervals[i-1][1]>=newInterval[0]:
-                    intervals[i-1][1]=max(intervals[i-1][1],newInterval[1])
-                else:
-                    intervals.insert(i,newInterval)
-                    i=i+1
-                f=1
-            
-            elif f==1:
-                if intervals[i-1][1]>=intervals[i][0]:
-                    intervals[i-1][1]=max(intervals[i-1][1],intervals[i][1])
-                    intervals.pop(i)
-                else:
-                    return intervals
-            else:
-                i+=1
-        if f==0:
-            intervals.append(newInterval)
-            if intervals[i-1][1]>=intervals[i][0]:
+            if intervals[i][0]<=intervals[i-1][1]:
                 intervals[i-1][1]=max(intervals[i-1][1],intervals[i][1])
                 intervals.pop(i)
+
+            else:
+                i+=1
         return intervals
+
+        
             
                 
 
