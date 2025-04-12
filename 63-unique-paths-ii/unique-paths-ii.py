b9ddbe3c5c914eb2c47ea0a79faffd384af5929b
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-04-12 19:32:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.51%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,34 +1,25 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        grid=obstacleGrid
-        m=len(grid)
-        n=len(grid[0])
-        dp=[[0]*n for _ in range(m)]
+        m=len(obstacleGrid)
+        n=len(obstacleGrid[0])
+        dp=[[0]*n for i in range(m)]
+        print(dp)
+        dp[0][0]=1
         for i in range(m):
-            if grid[i][0]==1 or dp[i-1][0]==-1:
-                dp[i][0]=-1
-            else:
-                dp[i][0]=1
-        for j in range(n):
-            if grid[0][j]==1 or dp[0][j-1]==-1:
-                dp[0][j]=-1
-            else:
-                dp[0][j]=1
-        for i in range(1,m):
-            for j in range(1,n):
-                if grid[i][j]==1:
-                    dp[i][j]=-1
+            for j in range(n):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
                     continue
-                if dp[i-1][j]==-1 and dp[i][j-1]==-1:
-                    dp[i][j]=-1
-                elif dp[i-1][j]==-1:
-                    dp[i][j]=dp[i][j-1]
-                elif dp[i][j-1]==-1:
-                    dp[i][j]=dp[i-1][j]
+                if i==0 and j==0:
+                    dp[i][j]=1
                 else:
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1]
-        if dp[-1][-1]==-1:
-            return 0
+                    if i==0:
+                        dp[i][j]=dp[i][j-1]
+                    elif j==0:
+                        dp[i][j]=dp[i-1][j]
+                    else:
+                        
+                        dp[i][j]=dp[i-1][j]+dp[i][j-1]
         return dp[-1][-1]
 
         
