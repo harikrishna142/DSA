9eb15092e4b0d09ce763086b94d010bc48938486
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-05-22 17:47:17 -0400

    Time: 1 ms (74.49%) | Memory: 17.8 MB (74.96%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -7,11 +7,8 @@ class Solution:
         for i in range(len(text)):
             if text[i] in p:
                 f[text[i]]+=1
-        print(f.values())
-        if f["l"]==min(f.values()) or f["o"]==min(f.values()):
-            return int((min(f.values()))//2)
-        else:
-            return min(min(f.values()),int((min(f["l"],f["o"]))//2))
+        
+        return min(min(f.values()),int((min(f["l"],f["o"]))//2))
 
 
 
