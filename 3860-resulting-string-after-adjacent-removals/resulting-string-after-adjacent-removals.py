ac76aa29370d50685219d0d70cdf374dbffeaa5d
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2025-06-01 17:24:31 -0400

    Time: 657 ms (60.60%) | Memory: 19.5 MB (71.76%) - LeetSync

--- 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
+++ 3860-resulting-string-after-adjacent-removals/resulting-string-after-adjacent-removals.py
@@ -1,17 +1,11 @@
 class Solution:
     def resultingString(self, s: str) -> str:
-        i=0
         st=[s[0]]
         for i in range(1,len(s)):
-            if len(st)==0:
+            if len(st)==0 or abs(ord(s[i])-ord(st[-1])) not in [1,25]:
                 st.append(s[i])
-                continue
-            if abs(ord(s[i])-ord(st[-1])) in [1,25]:
-                st.pop()
             else:
-                st.append(s[i])
-            
-                
+                st.pop()   
         return "".join(st)
 
         
