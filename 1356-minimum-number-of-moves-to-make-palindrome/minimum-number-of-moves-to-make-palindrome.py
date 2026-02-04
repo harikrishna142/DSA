dacf7e45397b899641f90d524a53f172cbb52ae7
Harikrishna <43600613+harikrishna142@users.noreply.github.com>
2026-02-03 19:16:53 -0500

    Time: 419 ms (12.76%) | Memory: 19.2 MB (60.91%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.py
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.py
@@ -2,30 +2,30 @@ class Solution:
     def minMovesToMakePalindrome(self, s: str) -> int:
         s = list(s)
         moves = 0
-        left, right = 0, len(s) - 1
-
-        while left < right:
-            # If characters match, move inward
-            if s[left] == s[right]:
-                left += 1
-                right -= 1
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            # Find the rightmost character index 'k' that matches s[l]
+            k = r
+            while k > l and s[k] != s[l]:
+                k -= 1
+            
+            if k == l:
+                # Case: s[l] is the unique odd-count character.
+                # We swap it with its right neighbor to push it toward the center.
+                s[l], s[l+1] = s[l+1], s[l]
+                moves += 1
+                # Note: We do NOT increment l or decrement r here because
+                # we haven't successfully matched s[l] yet.
             else:
-                # Find matching character for s[left] from the right side
-                k = right
-                while k > left and s[k] != s[left]:
-                    k -= 1
-
-                if k == left:  
-                    # No matching char found; move this char towards the center
-                    s[left], s[left + 1] = s[left + 1], s[left]
+                # Case: Found a match at index k.
+                # Bubble s[k] to the position r
+                while k < r:
+                    s[k], s[k+1] = s[k+1], s[k]
+                    k += 1
                     moves += 1
-                else:
-                    # Bring the matching char to the right position
-                    while k < right:
-                        s[k], s[k + 1] = s[k + 1], s[k]
-                        k += 1
-                        moves += 1
-                    left += 1
-                    right -= 1
-
+                # Now that the boundary s[l]...s[r] is palindromic, shrink window
+                l += 1
+                r -= 1
+                
         return moves
